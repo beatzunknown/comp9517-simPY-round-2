988800d2a6e87ec88097ad6a9802070ee32f4d2d
Ian <ianng2000@gmail.com>
2021-04-17 21:40:14 +1000

    format data

--- lanedetect.py
+++ lanedetect.py
@@ -116,8 +116,11 @@ def hough_lines(img, drawlinesOg=False):
     lines = cv2.HoughLinesP(img, rho, theta, threshold, np.array([]))
     line_img = np.zeros((*img.shape, 3), dtype=np.uint8)
 
+    lanes = None
+    h_samples = None
+
     if drawlinesOg:
-        draw_lines(line_img, lines)
+        lanes, h_samples = draw_lines(line_img, lines)
     else:
         sectionVal = 1280 // 20
         sections = [i for i in range(0, 1280 + 1, sectionVal)]
@@ -138,7 +141,7 @@ def hough_lines(img, drawlinesOg=False):
 
             draw_lines2(line_img, l)
 
-    return line_img
+    return line_img, lanes, h_samples
 
 
 def weighted_img(img, initial_img, α=0.8, β=1., λ=0.):
@@ -169,13 +172,7 @@ def extrapolateLine(img, gradient, x1, y1, color=[255, 0, 0], thickness=2):
         yPointOnTop = (gradient * topPoint) + c
 
     if (int(topPoint) in range(600, 750)):
-        print("hello", int(topPoint), int(pointOn720),
-              int(yPointOnTop), int(ypointOn720))
-
         return [int(pointOn720), int(ypointOn720), int(topPoint), int(yPointOnTop)]
-
-        cv2.line(img, (int(pointOn720), int(ypointOn720)),
-                 (int(topPoint), int(yPointOnTop)), color, thickness)
 
 
 def getmaxXLine(lines):
@@ -221,8 +218,6 @@ def draw_lines(img, lines, color=[255, 0, 0], thickness=2):
     except:
         pass
 
-    print(goodLines)
-
     linesOnSideLeft = [line for line in goodLines
                        if line != None and (line[0] == 0)]
 
@@ -238,6 +233,7 @@ def draw_lines(img, lines, color=[255, 0, 0], thickness=2):
     goodLinesOnLeft = []
     goodLinesOnRight = []
 
+    # for the lines on the bottom
     for i in range(0, 1152 + 1, 128):
         group = [line for line in linesOnBottom
                  if (line[1] == 720 and line[0]
@@ -250,16 +246,25 @@ def draw_lines(img, lines, color=[255, 0, 0], thickness=2):
             else:
                 goodLinesOnBottomRight.append(lineICareAbout)
 
+    linesInImage = [None, None, None, None]
+
     lineICareAbout = getmaxXLine(goodLinesOnBottomLeft)
     if lineICareAbout != None:
+        print("left")
+        linesInImage[1] = lineICareAbout
+
         cv2.line(img, (int(lineICareAbout[0]), int(lineICareAbout[1])),
                  (int(lineICareAbout[2]), int(lineICareAbout[3])), color, 10)
 
     lineICareAbout = getmaxXLine(goodLinesOnBottomRight)
     if lineICareAbout != None:
+        print("right")
+        linesInImage[2] = lineICareAbout
+
         cv2.line(img, (int(lineICareAbout[0]), int(lineICareAbout[1])),
                  (int(lineICareAbout[2]), int(lineICareAbout[3])), color, 10)
 
+    # lines on the left and the right sides
     for i in range(432, 576 + 1, 144):
         group = [line for line in linesOnSideLeft
                  if (line[1] in range(i, i + 144)) and 450 < line[1] < 600]
@@ -277,13 +282,22 @@ def draw_lines(img, lines, color=[255, 0, 0], thickness=2):
 
     lineICareAbout = getminYLine(goodLinesOnLeft)
     if lineICareAbout != None:
+        print("left side")
+        linesInImage[0] = lineICareAbout
         cv2.line(img, (int(lineICareAbout[0]), int(lineICareAbout[1])),
                  (int(lineICareAbout[2]), int(lineICareAbout[3])), color, 10)
 
     lineICareAbout = getminYLine(goodLinesOnRight)
     if lineICareAbout != None:
+        print("right side")
+
+        linesInImage[3] = lineICareAbout
         cv2.line(img, (int(lineICareAbout[0]), int(lineICareAbout[1])),
                  (int(lineICareAbout[2]), int(lineICareAbout[3])), color, 10)
+
+    lanes, h_samples = formatData(linesInImage)
+    print('there are', len(lanes), 'lanes in image')
+    return lanes, h_samples
 
 
 def draw_lines2(img, lines, color=[255, 0, 0], thickness=2):
@@ -387,10 +401,16 @@ def main():
         preprocessedImg = Preprocess(img)
         cannyImg = Canny(np.uint8(preprocessedImg))
         maskedImg = RegionOfInterest(cannyImg)
-        houged = hough_lines(maskedImg)
-        houged2 = hough_lines(maskedImg, True)
+        houged, lanes, h_samples = hough_lines(maskedImg)
+        houged2, lanes, h_samples = hough_lines(maskedImg, True)
+
         weighted = weighted_img(houged, img)
         weighted2 = weighted_img(houged2, img)
+
+        for l in lanes:
+            print(l)
+
+        print(h_samples)
 
         if (not os.path.exists('output')):
             print('creating output dir')
@@ -399,6 +419,53 @@ def main():
         cv2.imwrite("output/output" + str(i) + ".jpg", np.uint8(weighted))
         cv2.imwrite("output/output" + str(i) +
                     "DrawOriginal.jpg", np.uint8(weighted2))
+
+
+'''
+{
+  "lanes": [
+        [-2, -2, -2, -2, 632, 625, 617, 609, 601, 594, 586, 578, 570, 563, 555, 547, 539, 532, 524, 516, 508, 501, 493, 485, 477,
+            469, 462, 454, 446, 438, 431, 423, 415, 407, 400, 392, 384, 376, 369, 361, 353, 345, 338, 330, 322, 314, 307, 299],
+        [-2, -2, -2, -2, 719, 734, 748, 762, 777, 791, 805, 820, 834, 848, 863, 877, 891, 906, 920, 934, 949, 963, 978, 992, 1006, 1021,
+            1035, 1049, 1064, 1078, 1092, 1107, 1121, 1135, 1150, 1164, 1178, 1193, 1207, 1221, 1236, 1250, 1265, -2, -2, -2, -2, -2],
+        [-2, -2, -2, -2, -2, 532, 503, 474, 445, 416, 387, 358, 329, 300, 271, 241, 212, 183, 154, 125, 96, 67,
+            38, 9, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2],
+        [-2, -2, -2, 781, 822, 862, 903, 944, 984, 1025, 1066, 1107, 1147, 1188, 1229, 1269, -2, -2, -2, -2, -2, - \
+            2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2]
+       ],
+  "h_samples": [240, 250, 260, 270, 280, 290, 300, 310, 320, 330, 340, 350, 360, 370, 380, 390, 400, 410, 420, 430, 440, 450, 460, 470, 480, 490, 500, 510, 520, 530, 540, 550, 560, 570, 580, 590, 600, 610, 620, 630, 640, 650, 660, 670, 680, 690, 700, 710],
+  "raw_file": "path_to_clip"
+}
+'''
+
+
+def formatData(lines):
+    # print(lines)
+    h_samples = [i for i in range(240, 711, 10)]
+    # print(h_samples)
+    lanes = []
+
+    for l in lines:
+        # print(l)
+        if l != None:
+            x1, y1, x2, y2 = l
+            gradient, intercept = np.polyfit((x1, x2), (y1, y2), 1)
+            # y = mx + c
+            c = y1 - (gradient * x1)
+            lane = []
+
+            for y in h_samples:
+                if y < 360:
+                    lane.append(-2)
+                else:
+                    x_val = (y - c) // gradient
+                    lane.append(int(x_val))
+
+            lanes.append(lane)
+
+    return lanes, h_samples
+
+#formatData([1, 2, 3])
 
 
 main()
